ad4f2eb68509433f9cfb553c3587597e1a2e4037
Taras Boiko <kilotaras@gmail.com>
2015-12-01 13:22:00 +0200

    Syntax highlight for zsh

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -4,7 +4,7 @@ export ZSH=$HOME/.oh-my-zsh
 
 ZSH_THEME="amuse"
 
-plugins=(git autojump virtualenvwrapper pip)
+plugins=(git autojump virtualenvwrapper pip zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,2 +1,3 @@
 #/env/bin/bash
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
