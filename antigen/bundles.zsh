3a4a35902659367a8b35afbf291527ba960798bf
kilotaras <kilotaras@gmail.com>
2015-12-04 12:30:00 +0200

    Removed debian conflict with silver-searcherr

--- antigen/bundles.zsh
+++ antigen/bundles.zsh
@@ -1,14 +1,16 @@
 antigen bundles <<EOBUNDLES
   zsh-users/zsh-completions
   mercurial
-  debian
   ssh-agent
   cp
   pip
   python
   virtualenvwrapper
-  zsh-users/zsh-syntax-highlighting
-  extract
+   zsh-users/zsh-syntax-highlighting
+   extract
    zsh-users/zsh-history-substring-search
    autojump
+   unixorn/autoupdate-antigen.zshplugin
+   lesaint/lesaint-mvn
+   sharat87/pip-app
 EOBUNDLES
